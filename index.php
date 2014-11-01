ed65fa30b14d4b303fdc715c68e2b101fd761a9e
izabera <izaberina@gmail.com>
2014-11-01 19:53:26 -0400

    much more stilish

--- index.php
+++ index.php
@@ -3,7 +3,21 @@ if (isset($_POST['aringa'])) {
   $data = $_POST['aringa'];
   $file = tempnam(".", ""); //$file will now be '/var/www/arin.ga/public_html/XXXXXX'
   file_put_contents($file,$data);
-  echo "http://arin.ga".substr($file,28)."\n";
+  $file = substr($file,28);
+  if (preg_match("/^(Mozilla|Opera|Lynx)/", $_SERVER['HTTP_USER_AGENT'])) {
+    echo "<html>\n";
+    echo "  <head>\n";
+    echo "    <title>Aringa - $data</title>\n";
+    echo "    <link rel=\"stylesheet\" type=\"text/css\" href=\"http://arin.ga/style.css\">\n";
+    echo "  </head>\n";
+    echo "  <body>\n";
+    echo "    <table>\n";
+    echo "      <tr><td class=\"code\"><pre>Your file is here: <a href=\"http://arin.ga$file\">http://arin.ga$file</a></pre></td></tr>\n";
+    echo "    </table>";
+    echo "  </body>\n";
+    echo "</html>\n";
+  }
+  else echo "http://arin.ga".$file."\n";
 }
 //users going to arin.ga/XXXXXX are redirected to arin.ga/?b=XXXXXX if from browsers
 else if (isset($_GET['b'])) {
@@ -14,16 +28,24 @@ else if (isset($_GET['b'])) {
     die();
   }
   if (file_exists($data)) {
-    $loaded = file_get_contents($data);
     echo "<html>\n";
     echo "  <head>\n";
     echo "    <title>Aringa - $data</title>\n";
+    //echo "    <link rel=\"stylesheet\" type=\"text/css\" href=\"http://meyerweb.com/eric/tools/css/reset/reset.css\">\n";
     echo "    <link rel=\"stylesheet\" type=\"text/css\" href=\"http://arin.ga/style.css\">\n";
     echo "  </head>\n";
     echo "  <body>\n";
-    echo "    <xmp>";
-    echo `cat -n $data`; //horrible
-    echo "</xmp>\n";
+    $file = fopen($data, "r");
+    $count = 1;
+    echo "    <table>\n";
+    while ($line = fgets($file)) {
+      $line = str_replace("\n", "", $line);
+      $line = str_replace("\r", "", $line);
+      echo "      <tr><td class=\"num\"><xmp>$count.</xmp></td><td class=\"code\"><xmp>$line</xmp></td></tr>\n";
+      $count++;
+    }
+    echo "    </table>";
+    fclose($file);
     echo "  </body>\n";
     echo "</html>\n";
   }
